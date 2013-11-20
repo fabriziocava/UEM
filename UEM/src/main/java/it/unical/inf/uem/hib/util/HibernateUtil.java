72b10f3fb7983778e084ae2ea53508d27409539d
luigi <luigi@mok>
2013-11-20 20:37:54 +0100

    Problema in it.unical.inf.uem.hib.util.HibernateUtil.java NullPointExeption in ClassLoader.getSystemResource

--- UEM/src/main/java/it/unical/inf/uem/hib/util/HibernateUtil.java
+++ UEM/src/main/java/it/unical/inf/uem/hib/util/HibernateUtil.java
@@ -1,5 +1,11 @@
 package it.unical.inf.uem.hib.util;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Enumeration;
+
 import it.unical.inf.uem.hib.domain.City;
 
 import org.hibernate.SessionFactory;
@@ -11,13 +17,14 @@ public class HibernateUtil {
 
 	static{
 		try{
+			URL systemResource = ClassLoader.getSystemResource("/media/luigi/Data/Dropbox/eclipseJob/repositoryGit/UEM/UEM/src/main/java/resources/hibernate.cfg.xml");
 			sessionFactory=new Configuration()
-			.configure(ClassLoader.getSystemResource("resources/hibernate.cfg.xml"))
-			.addPackage("it.unical.mat.domain")
+			.addPackage("it.unical.inf.uem.hib.domain")
+			.configure(systemResource)
 			.addAnnotatedClass(City.class)
 			.buildSessionFactory();
 		}catch(Exception e){
-
+			e.printStackTrace();
 		}
 	}
 	//si devono aggiungere tutte le classi che vogliamo siano utilizzate nel db
--- UEM/src/main/java/it/unical/inf/uem/mvc/controller/HomeController.java
+++ UEM/src/main/java/it/unical/inf/uem/mvc/controller/HomeController.java
@@ -1,8 +1,8 @@
 package it.unical.inf.uem.mvc.controller;
 
-//import it.unical.inf.uem.hib.dao.CityDao;
-//import it.unical.inf.uem.hib.dao.imp.CityDaoImpl;
-//import it.unical.inf.uem.hib.domain.City;
+import it.unical.inf.uem.hib.dao.CityDao;
+import it.unical.inf.uem.hib.dao.imp.CityDaoImpl;
+import it.unical.inf.uem.hib.domain.City;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -37,27 +37,27 @@ public class HomeController {
 
 		model.addAttribute("serverTime", formattedDate );
 
-//		CityDao cityDao=new CityDaoImpl();
-//		Long id1=cityDao.saveCity("Acri");
-//		Long id2=cityDao.saveCity("Acri2");
-//		Long id3=cityDao.saveCity("Acri3");
-//		Long id4=cityDao.saveCity("Acri4");
-//
-//		cityDao.listCities(); ///tutte le citta
-//
-//		City idc1=cityDao.getCityWithId((long) 1);
-//		City idc3=cityDao.getCityWithId((long) 3);
-//
-//		City idc2=cityDao.getCityWithId((long) 2);
-//		City idc4=cityDao.getCityWithId((long) 4);
-//
-//		cityDao.updateCity(idc4.getId(), "Acri5");
-//
-//		cityDao.listCities(); // modifica della 4 in 5
-//
-//		cityDao.deleteCity(idc2.getId());
-//
-//		cityDao.listCities(); // eliminazione della 2
+		CityDao cityDao=new CityDaoImpl();
+		Long id1=cityDao.saveCity("Acri");
+		Long id2=cityDao.saveCity("Acri2");
+		Long id3=cityDao.saveCity("Acri3");
+		Long id4=cityDao.saveCity("Acri4");
+
+		cityDao.listCities(); ///tutte le citta
+
+		City idc1=cityDao.getCityWithId((long) 1);
+		City idc3=cityDao.getCityWithId((long) 3);
+
+		City idc2=cityDao.getCityWithId((long) 2);
+		City idc4=cityDao.getCityWithId((long) 4);
+
+		cityDao.updateCity(idc4.getId(), "Acri5");
+
+		cityDao.listCities(); // modifica della 4 in 5
+
+		cityDao.deleteCity(idc2.getId());
+
+		cityDao.listCities(); // eliminazione della 2
 
 		return "home";
 	}
