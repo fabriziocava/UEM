dca9f8ef6bfb50ec55dff09ee23d3d1eb7e744a2
luigi <luigi@mok>
2013-11-20 18:43:51 +0100

    OK

--- UEM/src/main/java/it/unical/inf/uem/mvc/controller/HomeController.java
+++ UEM/src/main/java/it/unical/inf/uem/mvc/controller/HomeController.java
@@ -1,5 +1,9 @@
 package it.unical.inf.uem.mvc.controller;
 
+//import it.unical.inf.uem.hib.dao.CityDao;
+//import it.unical.inf.uem.hib.dao.imp.CityDaoImpl;
+//import it.unical.inf.uem.hib.domain.City;
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -16,24 +20,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
+
 		model.addAttribute("serverTime", formattedDate );
-		
+
+//		CityDao cityDao=new CityDaoImpl();
+//		Long id1=cityDao.saveCity("Acri");
+//		Long id2=cityDao.saveCity("Acri2");
+//		Long id3=cityDao.saveCity("Acri3");
+//		Long id4=cityDao.saveCity("Acri4");
+//
+//		cityDao.listCities(); ///tutte le citta
+//
+//		City idc1=cityDao.getCityWithId((long) 1);
+//		City idc3=cityDao.getCityWithId((long) 3);
+//
+//		City idc2=cityDao.getCityWithId((long) 2);
+//		City idc4=cityDao.getCityWithId((long) 4);
+//
+//		cityDao.updateCity(idc4.getId(), "Acri5");
+//
+//		cityDao.listCities(); // modifica della 4 in 5
+//
+//		cityDao.deleteCity(idc2.getId());
+//
+//		cityDao.listCities(); // eliminazione della 2
+
 		return "home";
 	}
-	
+
 }
