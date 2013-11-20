76c453835a655721cd0719041bcf3fd4463f5e74
luigi <luigi@mok>
2013-11-20 18:21:11 +0100

    OK first

---
+++ UEM/src/main/java/it/unical/inf/uem/hib/util/HibernateUtil.java
@@ -0,0 +1,30 @@
+package it.unical.inf.uem.hib.util;
+
+import it.unical.inf.uem.hib.domain.City;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory;
+
+	static{
+		try{
+			sessionFactory=new Configuration()
+			.configure(ClassLoader.getSystemResource("resources/hibernate.cfg.xml"))
+			.addPackage("it.unical.mat.domain")
+			.addAnnotatedClass(City.class)
+			.buildSessionFactory();
+		}catch(Exception e){
+
+		}
+	}
+	//si devono aggiungere tutte le classi che vogliamo siano utilizzate nel db
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+
+}
