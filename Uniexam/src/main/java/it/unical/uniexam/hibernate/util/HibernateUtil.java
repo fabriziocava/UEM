2c3dfe89ca6f3bd37416713ae2a9617757f554ab
Pino <giuseppe.lombardo88@gmail.com>
2014-01-16 12:17:58 +0100

    refreshing

--- Uniexam/src/main/java/it/unical/uniexam/hibernate/util/HibernateUtil.java
+++ Uniexam/src/main/java/it/unical/uniexam/hibernate/util/HibernateUtil.java
@@ -13,9 +13,9 @@ import it.unical.uniexam.hibernate.domain.RequestedCourse;
 //import it.unical.uniexam.hibernate.domain.Session;
 import it.unical.uniexam.hibernate.domain.Student;
 import it.unical.uniexam.hibernate.domain.User;
-import it.unical.uniexam.hibernate.domain.utility.CommentOfMessage;
+import it.unical.uniexam.hibernate.domain.utility.CommentOfPost;
 import it.unical.uniexam.hibernate.domain.utility.Email;
-import it.unical.uniexam.hibernate.domain.utility.MessageOfGroup;
+import it.unical.uniexam.hibernate.domain.utility.PostOfGroup;
 import it.unical.uniexam.hibernate.domain.utility.PhoneNumber;
 
 import org.hibernate.SessionFactory;
@@ -28,39 +28,39 @@ import org.hibernate.cfg.Configuration;
  */
 public class HibernateUtil {
 
-	private static SessionFactory sessionFactory;
+        private static SessionFactory sessionFactory;
 
-	static{
-		try{
-			//ClassLoader.getSystemResource("resources/hibernate.cfg.xml")
-			sessionFactory=new Configuration()
-			.configure("resources/hibernate.cfg.xml")
-			.addPackage("it.unical.uniexam.hibernate.domain")
-			.addAnnotatedClass(User.class)
-			.addAnnotatedClass(Department.class)
-			.addAnnotatedClass(RequestedCourse.class)
-			.addAnnotatedClass(Course.class)
-			.addAnnotatedClass(DegreeCourse.class)
-			.addAnnotatedClass(Appeal.class)
-			.addAnnotatedClass(Group.class)
-//			.addAnnotatedClass(Session.class)
-			.addAnnotatedClass(PhoneNumber.class)
-			.addAnnotatedClass(Email.class)
-			.addAnnotatedClass(CommentOfMessage.class)
-			.addAnnotatedClass(MessageOfGroup.class)
-			.addAnnotatedClass(Professor.class)
-			.addAnnotatedClass(Manager.class)
-			.addAnnotatedClass(Student.class)
-			.buildSessionFactory();
-		}catch(Exception e){
-			new MokException(e);
-		}
-	}
-	//si devono aggiungere tutte le classi che vogliamo siano utilizzate nel db
+        static{
+                try{
+                        //ClassLoader.getSystemResource("resources/hibernate.cfg.xml")
+                        sessionFactory=new Configuration()
+                        .configure("resources/hibernate.cfg.xml")
+                        .addPackage("it.unical.uniexam.hibernate.domain")
+                        .addAnnotatedClass(User.class)
+                        .addAnnotatedClass(Department.class)
+                        .addAnnotatedClass(RequestedCourse.class)
+                        .addAnnotatedClass(Course.class)
+                        .addAnnotatedClass(DegreeCourse.class)
+                        .addAnnotatedClass(Appeal.class)
+                        .addAnnotatedClass(Group.class)
+//                        .addAnnotatedClass(Session.class)
+                        .addAnnotatedClass(PhoneNumber.class)
+                        .addAnnotatedClass(Email.class)
+                        .addAnnotatedClass(CommentOfPost.class)
+                        .addAnnotatedClass(PostOfGroup.class)
+                        .addAnnotatedClass(Professor.class)
+                        .addAnnotatedClass(Manager.class)
+                        .addAnnotatedClass(Student.class)
+                        .buildSessionFactory();
+                }catch(Exception e){
+                        new MokException(e);
+                }
+        }
+        //si devono aggiungere tutte le classi che vogliamo siano utilizzate nel db
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+        public static SessionFactory getSessionFactory() {
+                return sessionFactory;
+        }
 
 
 }
