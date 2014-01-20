5ef467fbad8130099bd0ad85e491c0d184d38d67
Pino <giuseppe.lombardo88@gmail.com>
2014-01-20 19:43:29 +0100

    examsession dao + impl

--- Uniexam/src/main/java/it/unical/uniexam/hibernate/dao/impl/ExamSessionDAOimpl.java
+++ Uniexam/src/main/java/it/unical/uniexam/hibernate/dao/impl/ExamSessionDAOimpl.java
@@ -1,13 +1,19 @@
 package it.unical.uniexam.hibernate.dao.impl;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import it.unical.uniexam.MokException;
 import it.unical.uniexam.hibernate.dao.ExamSessionDAO;
 import it.unical.uniexam.hibernate.domain.Course;
 import it.unical.uniexam.hibernate.domain.DegreeCourse;
+import it.unical.uniexam.hibernate.domain.Department;
 import it.unical.uniexam.hibernate.domain.ExamSession;
 import it.unical.uniexam.hibernate.util.HibernateUtil;
 
@@ -35,8 +41,46 @@ public class ExamSessionDAOimpl implements ExamSessionDAO {
 
 	@Override
 	public boolean canRegisterCourse(Course c) {
-		// TODO Auto-generated method stub
-		return false;
+
+		  Session session = HibernateUtil.getSessionFactory().openSession();
+          Transaction transaction = null;
+          try {
+              transaction = session.beginTransaction();
+              DegreeCourse d=c.getDegreeCourse();
+              Date datacorrente=new Date();
+              Set<ExamSession> examsession=getExamsession();
+              for (ExamSession examSession2 : examsession) {
+				
+            	  if(examSession2.getDegreecourse()==d)
+            		  if(examSession2.getDataInizio().getTime()>=datacorrente.getTime() && examSession2.getDataFine().getTime()<=datacorrente.getTime())
+            			  return true;
+			}
+              transaction.commit();
+              
+      } catch (Exception e) {
+              transaction.rollback();
+      } finally {
+              session.close();
+      }
+      return false;
+	}
+
+	@Override
+	public Set<ExamSession> getExamsession() {
+
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Set<ExamSession> res = null;
+		try {
+			Query q = session.createQuery("from ExamSession");
+			@SuppressWarnings("unchecked")
+			List<ExamSession> list = q.list();
+			res = new HashSet<ExamSession>(list);
+		} catch (Exception e) {
+			new MokException(e);
+		} finally {
+			session.close();
+		}
+		return res;
 	}
 
 }
