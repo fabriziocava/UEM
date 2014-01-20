f6248e929f2fd8af3a813cc9606673d47a42b227
Pino <giuseppe.lombardo88@gmail.com>
2014-01-20 15:44:46 +0100

    last refreshing

---
+++ Uniexam/src/main/java/it/unical/uniexam/mvc/service/ManagerService.java
@@ -0,0 +1,35 @@
+package it.unical.uniexam.mvc.service;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+import it.unical.uniexam.hibernate.domain.Course;
+import it.unical.uniexam.hibernate.domain.DegreeCourse;
+import it.unical.uniexam.hibernate.domain.Manager;
+import it.unical.uniexam.hibernate.domain.User;
+
+
+public interface ManagerService extends UserService {
+	
+	public final static String MANAGER_HOME="manager/home";
+	public final static String MANAGER_QUERY_ID="idManager";
+	public static final String MANAGER_IMAGE = "manager/image";
+	public static final String MANAGER_ACCOUNT = "manager/account";
+	public static final String MANAGER_UPLOAD ="manager/upload";
+	public static final String MANAGER_EXAM ="manager/exam_session";
+	public static final String MANAGER_COURSE ="manager/course";
+	public static final String MANAGER_COURSELIST= "manager/course/list";
+	
+	public Manager getManager(Long idUser);
+	public Boolean streamImage(Manager m, OutputStream outputStream);
+	public void putImage(Manager m, InputStream is,int length);
+	public Map<String, String> getPersonalizzationValues(Long id);
+	public void updatePersonalizzationValues(String stringValues,Long id);
+	public Set<DegreeCourse> getAssociatedCourseWithDepartment(Long id);
+	
+	
+
+}
